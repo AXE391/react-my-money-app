e33e7b1abd9f1ac116add62bfde192058706dae3
aelmore49 <aelmore@pyxl.com>
2022-02-04 09:02:58 -0600

    test

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -16,15 +16,17 @@ export const useSignup = () => {
 		setIsPending(true);
 		try {
 			// Sign up user with the email and password they entered in
+			// projectAuth has a method to sign the user up called "createUserWithEmailAndPassword", which takes two arguments: email and password. It sends back a response.
 			const res = await projectAuth.createUserWithEmailAndPassword(
 				email,
 				password
 			);
+			// If the response is bad, throw an error
 			if (!res) {
 				throw new Error("Could not complete signup");
 			}
 
-			// Add display name to user
+			// Add display name to user object that is returned.
 			await res.user.updateProfile({ displayName });
 
 			// Dispatch LOG_IN action
@@ -32,9 +34,11 @@ export const useSignup = () => {
 				type: "LOG_IN",
 				payload: res.user,
 			});
-
+			// reset pending and error states
 			setIsPending(false);
 			setError(null);
+
+			// Catch error if there is one and reset error and pending states
 		} catch (err) {
 			console.log(err.message);
 			setError(err.message);
